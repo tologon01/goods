50c91d038c5a57e9d508c2e0e151a3fb1482a92a
tologon01 <77880104+tologon01@users.noreply.github.com>
2021-06-07 19:24:12 +0300

    add adding

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,4 +8,14 @@ def homepage():
     txt = f.readlines()
     return render_template('index.html', goods=txt)
  
-    
+
+@app.route("/add/", methods=["POST"])
+def add():
+    good = request.form["good"]
+    f = open('goods.txt', 'a+', encoding='utf-8')
+    f.write(good + '\n')
+    f.close()
+    return """
+        <h1>Инвентарь пополнен</h1>
+        <a href='/'>Домой</a>
+    """
